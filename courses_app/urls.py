8bb64c93ea4d917dcc997afc7d4870fe1a617c6f
AliRashki <ali0rainbow6@gmail.com>
2026-05-15 21:24:05 +0330

    add: Add New Course button to courses list page

--- courses_app/urls.py
+++ courses_app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
 
     path('list/', views.course_list, name='course_list'),
     path('detail/<int:id>/', views.course_detail, name='course_detail'),    
-    path('add', views.add_course, name='add_course'),    
+    path('add/', views.add_course, name='add_course'),
 ]  
